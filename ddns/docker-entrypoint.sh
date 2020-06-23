01c8210d88959cb000066eff4b8e1d1ab0062379
ztechstack <ztechstack@outlook.com>
2020-06-23 19:22:40 +0800

    fix ddns add proxy env

--- ddns/docker-entrypoint.sh
+++ ddns/docker-entrypoint.sh
@@ -2,9 +2,22 @@
 
 echo "RECOVRY CRONTAB FILE"
 CRON_DDNS_CONF=$CRON_CONF/ddns
+CRON_DDNS_ENV=/usr/local/bin/ddns-env
 
 if [ ! -f "$CRON_DDNS_CONF" ]; then
     touch $CRON_DDNS_CONF
 fi
+
+
+if [ -n "$PROXY" ]; then
+    echo "#/bin/bash" > $CRON_DDNS_ENV
+    echo "PROXY=\"$PROXY\"" >> $CRON_DDNS_ENV
+fi
+
+if [ ! -f "$CRON_DDNS_ENV" ]; then
+    echo "#/bin/bash" > $CRON_DDNS_ENV
+fi
+
+chmod +x $CRON_DDNS_ENV
 
 exec "$@"
