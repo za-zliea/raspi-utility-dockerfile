9adc06ce33326b2de83efebfa4795641051d5bf0
ztechstack <ztechstack@outlook.com>
2021-10-30 23:46:04 +0800

    update

--- aria2/build.sh
+++ aria2/build.sh
@@ -4,7 +4,7 @@ source ./../env.sh
 
 cd "$(dirname "$0")"
 
-ARIA2_VERSION=1.35.0
+ARIA2_VERSION=1.36.0
 BUILD_ARG="${BUILD_ARG} --build-arg ARIA2_VERSION=${ARIA2_VERSION}"
 
 docker build ${BUILD_ARG} -t ${REGISTRY}/aria2:${ARIA2_VERSION} .
--- music-unlock/build.sh
+++ music-unlock/build.sh
@@ -4,7 +4,7 @@ source ./../env.sh
 
 cd "$(dirname "$0")"
 
-MUSIC_UNLOCK_VERSION=v1.8.1
+MUSIC_UNLOCK_VERSION=v1.9.1
 BUILD_ARG="${BUILD_ARG} --build-arg MUSIC_UNLOCK_VERSION=${MUSIC_UNLOCK_VERSION}"
 
 docker build ${BUILD_ARG} -t ${REGISTRY}/music-unlock:${MUSIC_UNLOCK_VERSION} .
